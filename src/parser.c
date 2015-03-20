da23fa7bdf21bba01220af8abbd73a6b572574eb
Blake Loring <blake_l@parsed.uk>
2015-03-20 15:21:32 +0000

    Implement parseClause

--- src/parser.c
+++ src/parser.c
@@ -1,30 +1,89 @@
 #include "parser.h"
 #include <ctype.h>
 
-char const* skipWhitespace(char const* satString) {
+char const* nextToken(char const* satString) {
   for (; *satString && isspace(*satString); satString++) {}
   return satString;
 }
 
-char const* parseClause(SAT* sat, char const* satString) {
+char const* parseVariable(SAT* sat, char const* satString) {
   
 }
 
-bool parseSat(SAT* sat, char const* satString) {
-  satString = skipWhitespace(satString);
+char const* parseClause(SAT* sat, char const* satString) {
+  
+  //Parse the first variable in clause
+  satString = parseVariable(satString);
+  if (!satString) {
+    return 0;
+  }
+
+  if (*satString == 'v') {
+    satString = nextToken(satString+1);
+  } else {
+    printf("Unexpected %c\n", *satString);
+    return 0;
+  }
+  
+  //Parse the second variable in clause
+  satString = parseVariable(satString);
+  if (!satString) {
+    return 0;
+  }
+
+  if (*satString == 'v') {
+    satString = nextToken(satString+1);
+  } else {
+    printf("Unexpected %c\n", *satString);
+    return 0;
+  }
+  
+  //Parse the last variable in clause
+  satString = parseVariable(satString);
+  if (!satString) {
+    return 0;
+  }
+  
+  return satString;
+}
+
+char const* parseCnfClause(SAT* sat, char const* satString) {
+
+  //Parse a clause
   if (*satString == '(') {
-    satString = parseClause(sat, satString+1);
+
+    //Parse the variables
+    satString = parseClause(sat, nextToken(satString+1));
     if (!satString) {
-      return false;
+      return 0;
     }
+    
+    //Check that it is closed
     if (*satString == ')') {
-      satString++;
+      satString = nextToken(satString+1);
     } else {
       printf("ERROR: Expecting )\n");
-      return false;
+      return 0;
     }
+
   } else {
     printf("ERROR: Expecting (\n");
-    return false;
+    return 0;
   }
+
+  //If the next terminal is a ^ then repeat for the next cnf clause  
+  if (*satString == '^') {
+    return parseCnfClause(sat, nextToken(satString+1));
+  } else if (!*satString) {
+    //If its the end of the parsing then return satString (Anything other than 0 is ok - 0 indicates error)
+    return satString;
+  } else {
+    //Any other characters cause an error
+    printf("ERROR: Unexpected %c\n", *satString);
+    return 0;
+  }
+}
+
+bool parseSat(SAT* sat, char const* satString) {
+  return parseCnfClause(sat, satString) != 0;
 }
