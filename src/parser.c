a9fcfdea4eec8c89747829af0b4a7bfba2277c23
Blake Loring <blake_l@parsed.uk>
2015-03-20 14:46:29 +0000

    Added some parsing helper functions

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,16 @@
 #include "parser.h"
+#include <ctype.h>
+
+char const* skipWhitespace(char const* satString) {
+  for (; *satString && isspace(*satString); satString++) {}
+  return satString;
+}
+
+char const* nextTerminal(char const* satString) {
+  return skipWhitespace(satString+1);
+}
 
 bool parseSat(SAT* sat, char const* satString) {
+  for (satString = skipWhitespace(satString); *satString; satString = nextTerminal(satString)) {
+  }
 }
